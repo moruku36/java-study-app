fc4cdd8b3de107a3d4879812c2746e5ae8881d8f
SeaOtter(Ken) <kentaro.mori36@gmail.com>
2025-08-04 15:03:58 +0900

    本番環境修正: データソース設定を簡素化し、Spring Boot自動設定を使用

--- src/main/java/com/studyapp/config/DatabaseConfig.java
+++ src/main/java/com/studyapp/config/DatabaseConfig.java
@@ -1,46 +1,9 @@
 package com.studyapp.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-import javax.sql.DataSource;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 @Configuration
 public class DatabaseConfig {
-    
-    @Value("${spring.datasource.url:}")
-    private String databaseUrl;
-    
-    @Bean
-    @Profile("production")
-    public DataSource dataSource() throws URISyntaxException {
-        if (databaseUrl != null && !databaseUrl.isEmpty() && !databaseUrl.contains("h2")) {
-            URI dbUri = new URI(databaseUrl);
-            
-            String username = dbUri.getUserInfo().split(":")[0];
-            String password = dbUri.getUserInfo().split(":")[1];
-            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-            
-            org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
-            dataSource.setDriverClassName("org.postgresql.Driver");
-            dataSource.setUrl(dbUrl);
-            dataSource.setUsername(username);
-            dataSource.setPassword(password);
-            dataSource.setTestOnBorrow(true);
-            dataSource.setTestWhileIdle(true);
-            dataSource.setTestOnReturn(true);
-            dataSource.setValidationQuery("SELECT 1");
-            dataSource.setMaxActive(10);
-            dataSource.setMinIdle(2);
-            dataSource.setMaxIdle(5);
-            
-            return dataSource;
-        }
-        
-        return null;
-    }
+    // Spring Bootの自動設定を使用
+    // application-production.ymlでデータソース設定を管理
 } 
