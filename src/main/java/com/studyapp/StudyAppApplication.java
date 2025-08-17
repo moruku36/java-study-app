7f3154694e8c14d8e03c1908edc796a1c5c41155
SeaOtter(Ken) <kentaro.mori36@gmail.com>
2025-08-17 15:26:32 +0900

    Add Actuator and startup diagnostics; expose /actuator/startup; disable dev auto-restart for faster startup

--- src/main/java/com/studyapp/StudyAppApplication.java
+++ src/main/java/com/studyapp/StudyAppApplication.java
@@ -2,6 +2,7 @@ package com.studyapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication(exclude = {
     org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
@@ -9,6 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudyAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StudyAppApplication.class, args);
+        SpringApplication app = new SpringApplication(StudyAppApplication.class);
+        // Collect detailed startup steps for /actuator/startup
+        app.setApplicationStartup(new BufferingApplicationStartup(2048));
+        app.run(args);
     }
 } 
